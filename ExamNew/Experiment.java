bced46a4cac37082de8ba61e26a64298e7429eb1
emsk <emsk@itu.dk>
2020-11-26 08:37:34 +0100

    Added comments

--- ExamNew/Experiment.java
+++ ExamNew/Experiment.java
@@ -20,37 +20,38 @@ public class Experiment {
    
 
     public static void main(String args[]) {
-        systemInfo();
-        warmUp();
-        createDir();
+        systemInfo();                                                       // Prints info about the machine doing the experiment
+        warmUp();                                                           // Warmup for the Benchmark                                                              
+        createDir();                                                        // Creates a directory string for test data
 
         // RUNNING THE EXPERIMENT
         System.out.println();
         System.out.println("Running the experiment...");
         
-        for (int i = 0; i < modeArray.length; i=i+2) {
-            FileWriter[] file = new FileWriter[algorithms.length];
-            StringBuilder[] sb = new StringBuilder[algorithms.length];
-            try   {
+        for (int i = 0; i < modeArray.length; i=i+2) {                      // For each Mode
+            FileWriter[] file = new FileWriter[algorithms.length];          
+            StringBuilder[] sb = new StringBuilder[algorithms.length];      
+
+            try   {                                                         // Try/catch for FileWriter
                 System.out.println();
-                for (int a = 0; a < algorithms.length; a++) {
-                    file[a] = createFile(a, i);
-                    sb[a] = new StringBuilder();
+                for (int a = 0; a < algorithms.length; a++) {               // FOR EACH ALGORITHM ////////////
+                    file[a] = createFile(a, i);                             // FileWriter for each algorithm
+                    sb[a] = new StringBuilder();                            // Stringbuilder for each algorithm
                 }
 
                 System.out.println("All Algorithms with mode: " + modeArray[i]);
                 
-                int[] seedArray = Seed.createSeed(seed);
-                for (int l = 0; l < seedArray.length; l++) {
-                    for (int j = 0; j < N.length; j++) {
+                int[] seedArray = Seed.createSeed(seed);                                    
+                for (int l = 0; l < seedArray.length; l++) {                 // FOR EACH SEED ///////////////
+                    for (int j = 0; j < N.length; j++) {                     // FOR EACH N     /////////////
                         System.out.println("-------------------------------------");
                         System.out.println("N: " + N[j] + " and Seed: " + seedArray[l]);
                         System.out.println("-------------------------------------");
+
                         double[] mean = new double[algorithms.length];
                         double[] sDev = new double[algorithms.length];
 
-                        for (int k = 0; k < runPerSeed; k++) {
-                            
+                        for (int k = 0; k < runPerSeed; k++) {              // We run each SEED and N several times
                             String inputArray = Producer.generate(createTestData(i,   j, seedArray[l]));
                             String inputPred  = Producer.generate(createTestData(i+1, j, seedArray[l]));
 
@@ -59,43 +60,48 @@ public class Experiment {
                             double[] tempMean = Benchmark.getMean();
                             double[] tempSdev = Benchmark.getSdev();
 
-                            for (int m = 0; m < algorithms.length; m++) {
+                            for (int m = 0; m < algorithms.length; m++) {   // Adding up all the mean 
                                 mean[m] += tempMean[m];
                                 sDev[m] += tempSdev[m];
                             }
                         }
                         for(int n = 0; n < algorithms.length; n++){
-                            sb[n].append(N[j] + " " + (mean[n] / runPerSeed) + " " + (sDev[n] / runPerSeed) + "\n");
+                            double totalMean = mean[n] / runPerSeed;        // Dividing mean with RunPerSeed
+                            double totalSdev = sDev[n] / runPerSeed;        // Dividing sDev with RunPerSeed
+                            sb[n].append(N[j] + " " + totalMean + " " +     // Adding test data with N, mean, sDev
+                                                    totalSdev + "\n");
                         }
                     }
                 }
-                addMeasurements(sb, file);
-            } catch (IOException e) { e.printStackTrace();}
+                addMeasurements(sb, file);                                  // Adding all the measurements to the data files
+            } catch (IOException e) { e.printStackTrace();}                 // catch for the FileWriter
         }
-        end();
+        end();                                                              // Prints end-statment for the experiment
     }
-
-
-// HELPER FUNCTIONS ///////////////////////////////////////////////////////////////////////////////////
-
-public static void warmUp() {
-
-    System.out.println("Running the warm-up...");
-    String correctness = "";
-    for (int i = 0; i < modeArray.length; i += 2) {
-        
-        for (int j = 0; j < 2; j++) {                                           // Running warmup for 2 first size N 
-            String inputArray = Producer.generate(createTestData(i,   j, seed));
-            String inputPred  = Producer.generate(createTestData(i+1, j, seed));
-
-            correctness = Benchmark.warmUp(inputArray, inputPred, 
-                                            (iterations * n), algorithms);
-        } 
-        System.out.println("Warm-up " + i+1 + "/" + modeArray.length/2 + " done! " + correctness);
-    }
-}
 
     /**
+     * Warms up the Benchmarking iterations * n times
+     */
+    public static void warmUp() {
+
+        System.out.println("Running the warm-up...");
+        String correctness = "";
+        for (int i = 0; i < modeArray.length; i += 2) {
+            
+            for (int j = 0; j < 2; j++) {                                           // Running warmup for 2 first size N 
+                String inputArray = Producer.generate(createTestData(i,   j, seed));
+                String inputPred  = Producer.generate(createTestData(i+1, j, seed));
+
+                correctness = Benchmark.warmUp(inputArray, inputPred, 
+                                                (iterations * n), algorithms);
+            } 
+            System.out.println("Warm-up " + i+1 + "/" + modeArray.length/2 + " done! " + correctness);
+        }
+    }
+    
+// HELPER FUNCTIONS ///////////////////////////////////////////////////////////////////////////////////
+
+    /** Adds all the test data to the the file and closes the FileWriter
      * 
      * @param sb a StringBuilder array
      * @param fw a FileWriter array
@@ -109,7 +115,7 @@ public static void warmUp() {
         }
     }
 
-    /**
+    /** Creates a directory that will contain all the test data files
      * 
      * @return a string for the newly created directory
      */
@@ -121,7 +127,7 @@ public static void warmUp() {
         return dir;
     }
 
-    /**
+    /** Creates Files for each algorithm and mode
      * 
      * @param s String of the directory
      * @param a index of the current algorithm
@@ -138,7 +144,7 @@ public static void warmUp() {
         return writer;
     }
 
-    /**
+    /** Creates the String[] that will be passed to the producer
      * 
      * @param j     index of the current mode
      * @param k     index of the current N
@@ -173,11 +179,11 @@ public static void warmUp() {
           System.out.println("-------------------------------------");
           System.out.println();
       }
-      /**
-       * Prints a statement that the test has finished and where to find
-       * the test data
-       */
-      public static void end(){
+    /**
+     * Prints a statement that the test has finished and where to find
+     * the test data
+     */
+    public static void end(){
         System.out.println("-------------------------------------");
         System.out.println("Experiment all Done!");
         System.out.println("Find the results here:");
